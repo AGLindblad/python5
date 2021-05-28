7fbb293007b8dd470d97e4c36961b5baa55281c5
Anders Lindblad <agl1983@yahoo.com>
2021-05-28 16:19:20 +0300

    more testing

--- teejo/aamutesti.py
+++ teejo/aamutesti.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms.ext.sqlalchemy.orm import model_form
 
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import StringField, PasswordField, validators
 app = Flask(__name__)
 app.secret_key = "arfhgqary04t4t9qg4qg9"
 db = SQLAlchemy(app)
@@ -13,11 +15,84 @@ class Task(db.Model):
 
 TaskForm = model_form(Task, base_class=FlaskForm, db_session=db.session)
 
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String, nullable=False, unique=True)
+  passwordHash = db.Column(db.String, nullable=False)
+
+  def setPassword(self, password)
+    self.passwordHash = generate_password_hash(password)
+
+  def checkPassword(self, password):
+    return check_password_hash(self.passwordHash, password)
+
+class UserForm(FlaskForm):
+  email = StringField("email", validators=[validators.Email()])
+  password = PasswordField("password", validators=[validators.InputRequired()])
+
+def currentUser():
+  try:
+    uid = int(session["uid"])
+  except:
+    return None
+  return User.query.get(uid)
+
+app.jinja_env.globals["currentUser"] = currentUser
+
+@app.route("/user/login", methods =["GET", "POST"])
+def loginView():
+  form = UserForm()
+
+  if  form.validate_on_submit():
+      email = form.email.data
+      password = form.password.data
+
+      user = User.query.filter_by(email=email).first()
+      if not user:
+        flash("Login failed")
+        return redirect("/user/login")
+      if not user.checkPassword(password):
+        flash("Login failed")
+        return redirect("/user/login")
+
+      session ["uid"]=user.id
+      flash("suycces")
+      return redirect("/")
+  return render_template("login.html", form=form)
+
+@app.route("/user/register", methods=["GET", "POST"])
+def registerView():
+  form = UserForm()
+
+  if  form.validate_on_submit():
+      email = form.email.data
+      password = form.password.data
+
+      if User.query.filter_by(email=email).first():
+        flash("User alreday exists")
+        return redirect("/user/login")
+
+      user = User(email=email)
+      user.setPassword(password)
+
+      db.session.add(user)
+      db.session.commit()
+      flash("Register")
+      return redirect("/user/login")
+
+  return render_template("register.html", form=form)
+
+@app.route("/user/logout")
+def logoutView():
+  session["uid"] = None
+  flash("cya")
+  return redirect("/")
+
 @app.before_first_request
 def initDb():
   db.create_all()
 
-  task = Task(name="Too much 2 dooooo :(")
+  task = Task(name="A lot")
   db.session.add(task)
   db.session.commit()
 
